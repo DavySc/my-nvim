3d7ee4589ee88d8885ffb1cb46e0e73fa8bed291
DavySc <d.vdschoot@gmail.com>
2025-08-06 19:54:44 +0200

    rename

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -13,33 +13,34 @@ return {
       require('mason').setup()
 
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      -- LSP setup for Haskell
+
+      -- Haskell LSP
       require('lspconfig').hls.setup({capabilities = capabilities,})
 
-      -- Treesitter configuration for Haskell
+      -- C++ (and WASM via clangd) LSP
+      require('lspconfig').clangd.setup({
+        capabilities = capabilities,
+        -- Optionally, add cmd = { "clangd", "--compile-commands-dir=build" }, if needed
+      })
+
+      -- Treesitter configuration for Haskell, C, and C++
       require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'haskell' },
+        ensure_installed = { 'haskell', 'cpp', 'c' }, -- add other languages as needed
         highlight = { enable = true },
         indent = { enable = true },
         auto_install = true,
       }
 
-      -- conform.nvim for formatting with ormolu
+      -- Formatters: ormolu for Haskell and clang-format for C/C++
       require('conform').setup({
         formatters_by_ft = {
           haskell = { 'ormolu' },
+          cpp = { 'clang_format' },
+          c = { 'clang_format' },
         },
-        -- If you want to pass specific flags to ormolu:
-        -- formatters = {
-        --   ormolu = {
-        --     prepend_args = {
-        --       "--color", "always"
-        --     },
-        --   },
-        -- },
       })
 
-      -- Keybindings for Telescope fuzzy finder
+      -- Telescope keybindings
       local opts = { noremap = true, silent = true }
       vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', opts)
       vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', opts)
@@ -47,7 +48,4 @@ return {
       vim.api.nvim_set_keymap('n', '<leader>fm', '<cmd>Telescope marks<cr>', opts)
     end,
   },
-
 }
-
-
