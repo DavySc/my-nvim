d4eb4d372864678f8937ec43e51d1b6b934e02a4
DavySc <d.vdschoot@gmail.com>
2025-08-08 21:58:24 +0200

    Oil file navigation

--- init.lua
+++ init.lua
@@ -3,11 +3,11 @@ print("G'day sir!")
 require("config.lazy")
 
 vim.opt.shiftwidth = 4
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- execution keymaps
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":.lua<CR>")
+vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 vim.opt.number = true
 vim.opt.relativenumber = true
