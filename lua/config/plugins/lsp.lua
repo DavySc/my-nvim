1424e5f7845fc6045093218ae57a4cf2a8e27518
DavySc <d.vdschoot@gmail.com>
2025-08-10 17:05:58 +0200

    rustaceanvim

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -24,13 +24,12 @@ return {
 
       -- Treesitter configuration for Haskell, C, and C++
       require('nvim-treesitter.configs').setup {
-        ensure_installed = {'rust', 'haskell','html','css', 'cpp', 'c', 'regex', 'tsx', 'javascript'}, -- add other languages as needed
+        ensure_installed = {'markdown', 'markdown_inline', 'rust', 'haskell','html','css', 'cpp', 'c', 'regex', 'tsx', 'javascript'}, -- add other languages as needed
         highlight = { enable = true },
         indent = { enable = true },
         auto_install = true,
       }
       
-      require('lspconfig').rust_analyzer.setup({capabilities = capabilities})
 
       -- Formatters: ormolu for Haskell and clang-format for C/C++
       require('conform').setup({
