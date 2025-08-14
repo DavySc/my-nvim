0eff4a543237c0a0a21db8efee5b71e3d273118d
DavySc <d.vdschoot@gmail.com>
2025-08-14 22:23:25 +0200

    Removed veryl

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -44,24 +44,13 @@ return {
 
       -- Treesitter configuration for Haskell, C, and C++
       require('nvim-treesitter.configs').setup {
-        ensure_installed = {'markdown', 'markdown_inline', 'rust', 'haskell','html','css', 'cpp', 'c', 'regex', 'tsx', 'javascript', 'typescript'}, -- add other languages as needed
+        ensure_installed = { 'markdown', 'markdown_inline', 'rust', 'haskell','html','css', 'cpp', 'c', 'regex', 'tsx', 'javascript', 'typescript'}, -- add other languages as needed
         highlight = { enable = true },
         indent = { enable = true },
         auto_install = true,
       }
       
 
-require('lspconfig').veryl_ls.setup({
-  cmd = { 'veryl-ls' },
-  filetypes = { 'veryl' },
-  root_dir = require('lspconfig.util').root_pattern('Veryl.toml', '.git'),
-  on_attach = function(client, bufnr)
-    local opts = { noremap=true, silent=true, buffer=bufnr }
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-  end,
-})
       -- Formatters: ormolu for Haskell and clang-format for C/C++
       require('conform').setup({
         formatters_by_ft = {
